97e5f22ed86e79b2b11b01345c0731b1d81fc1e6
trianglegirl <r.davies3@lancaster.ac.uk>
2016-08-16 13:56:46 +0100

    Generalise for number of repeats

--- example_weighting_vs_repeating.R
+++ example_weighting_vs_repeating.R
@@ -1,21 +1,23 @@
 library(rARPACK)
 
+n1 = 1; n2 = 2
+
 A <- matrix(c(1,0.5,0.5,1),2,2)
 d <- rowSums(A)
 L <- (1/sqrt(d)) %*% t(1/sqrt(d))* A
 e <- eigen(L)$vectors
 
-s <- c(3,2)
+s <- c(n1,n2)
 Astar <- s%*%t(s) * A
 dstar <- rowSums(Astar)
 Lstar <- (1/sqrt(dstar)) %*% t(1/sqrt(dstar))* Astar
 estar <- eigen(Lstar)$vectors
 
-Arep <- matrix(0,5,5)  
-Arep[1:3,1:3] <- A[1,1]
-Arep[1:3,4:5] <- A[1,2]
-Arep[4:5,1:3] <- A[1,2]
-Arep[4:5,4:5] <- A[2,2]
+Arep <- matrix(0,(n1+n2),(n1+n2))  
+Arep[1:n1,1:n1] <- A[1,1]
+Arep[1:n1,(n1+1):(n1+n2)] <- A[1,2]
+Arep[(n1+1):(n1+n2),1:n1] <- A[1,2]
+Arep[(n1+1):(n1+n2),(n1+1):(n1+n2)] <- A[2,2]
 
 drep <- rowSums(Arep)
 Lrep <- (1/sqrt(drep)) %*% t(1/sqrt(drep))* Arep
